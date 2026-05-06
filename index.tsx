d21c7a9eb4cacb84dbdb9918413ad193633221af
rolfi <t.rolfsmeier@live.de>
2026-05-06 07:21:52 +0200

    fix: config resolution

--- index.tsx
+++ index.tsx
@@ -1,22 +1,14 @@
 #!/usr/bin/env bun
 
 import { App } from "@components/App";
-import {
-  ConfigProvider,
-  type CliConfig,
-  type Config,
-} from "@contexts/ConfigContext";
+import { ConfigProvider, type CliConfig } from "@contexts/ConfigContext";
 import { FocusProvider } from "@contexts/FocusContext";
 import { render } from "ink";
 import { merge } from "lodash";
-import { readFileSync } from "node:fs";
-import { resolve } from "node:path";
+import defaultConfig from "./config.json";
 
 // clear screen and move cursor
 process.stdout.write("\u001b[2J\u001b[0;0H");
-
-const configPath = resolve(import.meta.dir, "config.json");
-const defaultConfig: Config = JSON.parse(readFileSync(configPath, "utf-8"));
 
 const cliArgs = process.argv.slice(2);
 const initialSearchTerm = cliArgs.join(" ");
